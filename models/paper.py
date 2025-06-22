1d3e7bfdf51f14caf69fbe8e5610664e733002fa
helenfan1002 <rfan12@u.rochester.edu>
2025-06-23 00:33:53 +0800

    Update paper.py

--- models/paper.py
+++ models/paper.py
@@ -2,10 +2,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Paper:
+    paper_id: str
     title: str
     authors: list
     year: int
     abstract: str
     citation_count: int
 
-    
